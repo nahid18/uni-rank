d5389878c6ad39e1e8ba2783b51549fa17f87642
nahid18 <nahidpatwary50@email.com>
2022-07-21 16:04:34 +0600

    fixed api issue

--- unirank/rank/usa_rank.py
+++ unirank/rank/usa_rank.py
@@ -9,79 +9,76 @@ import os
 class Ranking:
     def __init__(self):
         self.api = "https://www.usnews.com/best-colleges/api/search?_sort=rank&_sortDirection=asc&schoolType=national-universities"
+        self.header = self._useragent()
+        self.dirname = "result"
 
 
     def _useragent(self):
         agents = pkgutil.get_data(__package__, "useragents.txt").decode("utf-8")
-        return random.choice(agents.splitlines())
-
-
-    def __save_initial(self):
-        usnews = header_selected = dict()
-        header_selected = dict()
-        fetched = False
-        while fetched != True:
-            try:
-                ua = self._useragent()
-                headers = {"User-Agent": ua}
-                data = requests.get(url=self.api, headers=headers, timeout=3)
-                if data.status_code == 200:
-                    header_selected = headers
-                    usnews = data.json()
-                    with open("apidata/1.json", 'w') as fw:
-                        json.dump(usnews, fw)
-                    fetched = True
-                    return {"totalPages": usnews['data']['totalPages'], "head": header_selected}
-            except:
-                pass
-
-
-    def __save_rest(self, length, head):
-        for page in range(2, length+1):
-            url = self.api+"&_page="+str(page)
-            try:
-                page_raw = requests.get(url=url, headers=head, timeout=3)
-                if page_raw.status_code == 200:
-                    page_data = page_raw.json()
-                    with open("apidata/"+str(page)+".json", 'w') as f:
-                        json.dump(page_data, f)
-            except:
-                pass
-
+        return {"User-Agent": random.choice(agents.splitlines())}
 
     def __read_json(self, filepath):
         with open(filepath, 'r') as fp:
-                return json.load(fp)
+            return json.load(fp)
 
 
-    def __check_directory(self, dirname):
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-            initials = self.__save_initial()
-            head = initials["head"]
-            self.__save_rest(initials['totalPages'], head)
+    def __download_pages(self, pages):
+        if not os.path.exists(self.dirname):
+            os.makedirs(self.dirname)
+        fetched = []
+        done = False
+        if pages != []:
+            while pages != fetched:
+                uncommon = set(pages).difference(set(fetched))
+                for page in list(uncommon):
+                    url = self.api+"&_page="+str(page)
+                    try:
+                        raw = requests.get(url=url, headers=self.header, timeout=3)
+                        if raw.status_code == 200:
+                            data = raw.json()["data"]
+                            out_file = f"{self.dirname}/{str(page)}.json"
+                            print(f"Saving page {str(page)} to {out_file}")
+                            with open(out_file, 'w') as f:
+                                json.dump(data, f)
+                            fetched.append(page)
+                    except:
+                        raise Warning(f"Could not fetch {str(page)}")
 
 
-    def get_usa(self):
-        dirname = 'apidata'
-        self.__check_directory(dirname)
+    def __parse_json(self):
         complete_list = list()
-        files = [int(i.split('/')[1].split('.')[0]) for i in glob(dirname+"/*.json")]
-        for num in sorted(files):
-            apifile = dirname+"/"+str(num)+".json"
-            file_json = self.__read_json(apifile)
-            file_data = file_json["data"]
-            for item in file_data["items"]:
+        numbers = sorted([int(i.split('/')[1].split('.')[0]) for i in glob(f"{self.dirname}/*.json")])
+        for num in numbers:
+            data = self.__read_json(filepath=f"{self.dirname}/{str(num)}.json")
+            for item in data["items"]:
                 out = dict()
                 detail = item["institution"]
                 item_keys = ["displayName", "rankingDisplayRank", "state", "city", "zip"]
                 for key in item_keys:
                     out[key] = detail[key]
-                out["description"] = desc = item["blurb"]
-                if desc.startswith("<p>"):
-                    out["description"] = desc[3:-4]
                 complete_list.append(out)
         return complete_list
+
+
+    def get_usa(self):
+        if not os.path.exists(self.dirname):
+            os.makedirs(self.dirname)
+        total_pages = 0
+        while total_pages == 0:
+            try:
+                raw = requests.get(url=self.api, headers=self.header, timeout=3)
+                if raw.status_code == 200:
+                    data = raw.json()["data"]
+                    total_pages = data["total_pages"]
+            except:
+                raise Exception("Failed to fetch data")
+
+        downloaded = [int(f.split('/')[-1].split('.')[0]) for f in glob(f"{self.dirname}/*json")]
+        remaining = list(set([i for i in range(1,total_pages+1)]) ^ set(downloaded))
+        self.__download_pages(pages=remaining)
+
+        data = self.__parse_json()
+        return data
 
 
     def get_names(self):
